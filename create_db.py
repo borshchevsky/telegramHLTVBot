4d64eb48dc12371882b4cda832aedae2fd8f0150
Pavel <3410914@gmail.com>
2020-06-20 14:54:42 +0300

    added # uri = 'sqlite:///' + os.path.join(basedir, 'base.db?check_same_thread=False')

--- create_db.py
+++ create_db.py
@@ -2,7 +2,6 @@ import os
 from models import Base, engine
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-uri = 'sqlite:///' + os.path.join(basedir, 'base.db')
 
 
 Base.metadata.create_all(engine)
--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 GROUP_ID = -443926074
 TEAM = 'vitality'
-INTERVAL = 1800  # раз в сколько времени (в секундах) проверять наличие матчей команды
+INTERVAL = 600.  # раз в сколько времени (в секундах) проверять наличие матчей команды
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-DB_URI = 'sqlite:///' + os.path.join(basedir, 'base.db')
+DB_URI = 'sqlite:///' + os.path.join(basedir, 'base.db?check_same_thread=False')
 ROLES = {'admin', 'user'}
